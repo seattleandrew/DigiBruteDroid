17357bb11df72547457735671e0780656b6589ce
s7560m <haydenhoffman@gmail.com>
2015-06-30 22:33:37 +0000

    FINAL COMMIT
    Changed multiple things to increase efficience of code. Diminished multiple lines of code. This is the final commit that basically polishes up the code from those multiple nested if statement blocks and uses multiple ternary operators to make it look more polished. Looks good so far.

--- DigiBruteDroid.ino
+++ DigiBruteDroid.ino
@@ -58,7 +58,9 @@ void loop() {
       if (b==10){
         a = (a==10) ? 0 : (a + 1);
         if (a==10){
-          e = (e==10 && key_stroke_e == true) ? 0 : (e + 1);
+          if (key_stroke_e == true){
+            e = (e==10) ? 0 : (e + 1);
+          }  
         }
       }
     }
